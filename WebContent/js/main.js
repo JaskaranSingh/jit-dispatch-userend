a97136ac745b1be1139467305c2192d0c3fd05e6
Jaskaran <lamba.jaskaran@gmail.com>
2015-11-03 05:03:06 -0800

    Pushes code at once

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,4 +1,5 @@
 var map = null;
+var markers= new Array();
 function initialize() {
     var mapCanvas = document.getElementById('map');
 		myLatLng = {lat: 17.440091, lng: 78.380193};
@@ -20,7 +21,7 @@ $("document").ready(function(){
 });
 
 function getTech(){
-  var custId = 2;
+  var cust_id = 2;
   var selectedType = document.getElementById("select-type").value;
   if(selectedType == ""){
     alert("Please select appropriate option");
@@ -34,8 +35,8 @@ function getTech(){
        
           // The data to send (will be converted to a query string)
           data: {
-              custID: custId,
-              type: selectedType,
+              cust_id: cust_id,
+              product_id: selectedType,
               format: "json",
               
           },
@@ -64,12 +65,13 @@ function getTech(){
        
           // Code to run regardless of success or failure
           complete: function( xhr, status ) {
-              alert( "The request is complete!" );
+              //alert( "The request is complete!" );
           }
       });
   }
 
 }
+
 function callbackForTechSupport(data){
 	var iconBase = 'http://maps.google.com/mapfiles/kml/shapes/man.png';
 	LatLng = {lat: data[0].CustomerLat, lng: data[0].CustomerLng};
@@ -82,10 +84,12 @@ function callbackForTechSupport(data){
 	
 	map.panTo(marker.getPosition());
 	smoothZoom(map, 14, map.getZoom());
+	deleteMarkers();
 	if(data.length >1) {
 		for(var i =1;i<data.length;i++)
 		{
 			LatLng = {lat:data[i].techLat,lng:data[i].techLng};
+			var tech_id= data[i].tech_id;
 			console.log(LatLng.lat);
 			console.log(map);
 			var iconBase = 'http://maps.google.com/mapfiles/kml/shapes/man.png';
@@ -96,19 +100,45 @@ function callbackForTechSupport(data){
 				    anchor: new google.maps.Point(0, 0) // anchor
 				};
 			var marker = new google.maps.Marker({
+				  id: tech_id,
 				  position: LatLng,
 				  map: map,
-				  title: "Technician",
+				  title: "Technician - "+ tech_id,
 				  icon: icon,
 				  size: new google.maps.Size(22, 32)
 				});
+			markers.push(marker);
 			marker.addListener('click', function() {
-			    popUp();
+			    popUp(this.id);
 			  })
 		}
 	}
 	
 }
+
+function confirmTechMessage(data){
+	var tech_id = data.tech_id;
+	var tech_name = data.tech_name;
+	
+	document.getElementById("container").innerHTML="<h3>Your Technician Details<br><br>Technician ID: "+tech_id+"<br>Technician Name: "+ tech_name+"</h3>";
+	
+	$('<div "></div>').appendTo('body')
+	  .html('<div ">Your Technician has been confirmed.</div>')
+	  .dialog({
+	      modal: true, title: 'Confirmed', zIndex: 10000, autoOpen: true,
+	      width: '300px', resizable: false,
+	      buttons: {
+	          OK: function () {
+//	              doFunctionForYes();
+	              $(this).dialog("close");
+	          }
+	      },
+	      close: function (event, ui) {
+	          $(this).remove();
+	      }
+	});
+}
+
 //the smooth zoom function
 function smoothZoom (map, max, cnt) {
     if (cnt >= max) {
@@ -123,7 +153,53 @@ function smoothZoom (map, max, cnt) {
     }
 } 
 
-function popUp(){
+function confirmTechnician(tech_id){
+	var cust_id = 2;
+	  var selectedType = document.getElementById("select-type").value;
+	$.ajax({
+	       
+         // The URL for the request
+         url: "http://113.128.164.168:8090/JITDispatch/Customer_Server",
+      
+         // The data to send (will be converted to a query string)
+         data: {
+        	 cust_id: cust_id,
+        	 product_id: selectedType,
+        	 tech_id: tech_id,
+             format: "json",
+             
+         },
+      
+         // Whether this is a POST or GET request
+         type: "GET",
+         crossDomain:true,
+         // The type of data we expect back
+         dataType : 'jsonp',
+         jsonp : false,
+         jsonpcallback: 'confirmTechMessage',
+
+      
+         // Code to run if the request succeeds;
+         // the response is passed to the function
+         
+      
+         // Code to run if the request fails; the raw request and
+         // status codes are passed to the function
+         error: function( xhr, status, errorThrown ) {
+//             alert( "Sorry, there was a problem!" );
+             console.log( "Error: " + errorThrown );
+             console.log( "Status: " + status );
+             console.dir( xhr );
+         },
+      
+         // Code to run regardless of success or failure
+         complete: function( xhr, status ) {
+             //alert( "The request is complete!" );
+         }
+     });
+}
+
+function popUp(tech_id){
     
 	
 	$('<div "></div>').appendTo('body')
@@ -133,6 +209,7 @@ function popUp(){
 	      width: '200px', resizable: false,
 	      buttons: {
 	          Yes: function () {
+	        	  confirmTechnician(tech_id);
 //	              doFunctionForYes();
 	              $(this).dialog("close");
 	          },
@@ -147,4 +224,15 @@ function popUp(){
 	});
 		
 }
-
+function setMapOnAll(map) {
+	  for (var i = 0; i < markers.length; i++) {
+	    markers[i].setMap(map);
+	  }
+	}
+function clearMarkers() {
+	  setMapOnAll(null);
+	}
+function deleteMarkers() {
+	  clearMarkers();
+	  markers = [];
+	}
